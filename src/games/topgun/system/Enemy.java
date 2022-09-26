f3f9150b80bfeedb7898b087c3860c4d7140ba66
Dorian Majano <dorian.majano@gmail.com>
2022-09-26 07:18:17 -0700

    Update enemy

--- src/games/topgun/system/Enemy.java
+++ src/games/topgun/system/Enemy.java
@@ -1,5 +1,57 @@
 package games.topgun.system;
 
-class Enemy extends Plane {
+import javax.swing.*;
 
+class Enemy extends Plane {
+    private Weapon weapon;
+
+    public Enemy(int x_coord, int y_coord) {
+        initEnemy(x_coord, y_coord);
+    }
+
+    private void initEnemy(int x_coord, int y_coord) {
+        setX_coordinate(x_coord);
+        setY_coordinate(y_coord);
+
+        weapon = new Weapon(x_coordinate, y_coordinate);
+
+        String enemyPath = "img/";
+        ImageIcon enemy = new ImageIcon(enemyPath);
+        setImage(enemy.getImage());
+    }
+
+    public void move(int direction) {
+        x_coordinate += direction;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public class Weapon extends Plane {
+        private boolean destroyed;
+
+        public Weapon(int x_coord, int y_coord) {
+            initWeapon(x_coord, y_coord);
+        }
+
+        private void initWeapon(int x_coord, int y_coord) {
+            setDestroyed(true);
+
+            x_coordinate = x_coord;
+            y_coordinate = y_coord;
+
+            // String weaponPath = "img/";
+            // ImageIcon weapon = new ImageIcon(weaponPath);
+            // setImage(weapon.getImage());
+        }
+
+        public void setDestroyed(boolean destroyed) {
+            this.destroyed = destroyed;
+        }
+
+        public boolean isDestroyed() {
+            return destroyed;
+        }
+    }
 }
